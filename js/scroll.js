84072b831c4c76a036cc4f37294d872e399e7370
Andrew <62069187+kondaaakov@users.noreply.github.com>
2021-08-14 15:21:31 +0300

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 let avatarEl = document.querySelector('#avatar');
-let pageHeight = document.body.scrollHeight,
+let pageHeight = document.body.scrollHeight + 239,
     windowHeight = window.innerHeight,
     maxScroll = pageHeight - windowHeight,
     minScroll = 0,
@@ -7,18 +7,10 @@ let pageHeight = document.body.scrollHeight,
     maxHeight = 120,
     dScroll = maxScroll - minScroll,
     dHeight = maxHeight - minHeight;
-
 window.addEventListener('scroll', () => {
     let currentScroll = window.pageYOffset;
-    console.log(currentScroll);
-    // if (avatarEl.clientHeight > minHeight) {
-        let currentSize = maxHeight - (dHeight * currentScroll) / dScroll;
+    let currentSize = maxHeight - (dHeight * currentScroll) / dScroll;
 
-        avatarEl.style.height = currentSize + 'px';
-        avatarEl.style.width = currentSize + 'px';
-    // }
+    avatarEl.style.height = avatarEl.style.width = currentSize + 'px';
 })
 
-console.log("Высота страницы: " + pageHeight);
-console.log("Высота окна: " + windowHeight);
-console.log("Максимальный скролл: " + maxScroll);
