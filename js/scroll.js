ff5c6e5b5d0532cd5c733f7e16c784d434025288
Andrew <62069187+kondaaakov@users.noreply.github.com>
2021-08-14 16:18:30 +0300

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,28 @@
 let avatarEl = document.querySelector('#avatar');
-let pageHeight = document.body.scrollHeight + 239,
+let avatarText = document.querySelector('#avatarText');
+
+let pageHeight = document.body.scrollHeight + 219,
     windowHeight = window.innerHeight,
     maxScroll = pageHeight - windowHeight,
     minScroll = 0,
     minHeight = 40,
     maxHeight = 120,
+    minMargin = 0,
+    maxMargin = -40,
+    minOpacity = 0,
+    maxOpacity = 1,
     dScroll = maxScroll - minScroll,
     dHeight = maxHeight - minHeight;
 window.addEventListener('scroll', () => {
     let currentScroll = window.pageYOffset;
+    console.log(currentScroll);
     let currentSize = maxHeight - (dHeight * currentScroll) / dScroll;
-
     avatarEl.style.height = avatarEl.style.width = currentSize + 'px';
+
+    let currentMargin = ((maxMargin - minMargin) * currentScroll) / dScroll + minMargin;
+    avatarEl.style.marginTop = currentMargin + 'px';
+
+    let currentOpacity = maxOpacity - ((maxOpacity - minOpacity) * currentScroll) / dScroll;
+    avatarText.style.opacity = currentOpacity;
 })
 
